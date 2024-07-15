62126b2c17fb302cba3f3e3f633956613e096e92
danmelo1994 <danilo94sm@outlook.com>
2024-07-14 21:49:52 -0300

    criacao das paginas

---
+++ src/pages/PaginaBase/index.js
@@ -0,0 +1,16 @@
+import styles from "./PaginaBase.module.css";
+import Cabecalho from "componentes/Cabecalho";
+import Rodape from "componentes/Rodape";
+import { Outlet } from "react-router-dom";
+
+function PaginaBase() {
+    return (
+        <main>
+            <Cabecalho />
+            <Outlet />
+            <Rodape />
+        </main>
+    )
+}
+
+export default PaginaBase;
